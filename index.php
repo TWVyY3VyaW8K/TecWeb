3beb05acf09f8215667b64a983db72d037b14f1b
Unknown <pardeeps460@gmail.com>
2018-12-10 22:53:55 +0100

    minor changes

--- index.php
+++ index.php
@@ -31,14 +31,16 @@
 			resetSessionPaginationNum('pagNum'.ucfirst(pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME)));
 	}
 	if(isset($_GET['lNumI'])){
-		$_SERVER['REQUEST_URI'] = removeqsvar($_SERVER['REQUEST_URI'], 'lNumI');
 		$_SESSION['backPageRedirect'] = $_SERVER['REQUEST_URI'];
 		$numI = (intval(htmlspecialchars($_GET['lNumI'], ENT_QUOTES, "UTF-8")) >= 1) ? intval(htmlspecialchars($_GET['lNumI'], ENT_QUOTES, "UTF-8")) : 0;
 		if($numI != 0){
 				$_SESSION['giveLike'] = $numI;
 		}
+		$_SERVER['REQUEST_URI'] = removeqsvar($_SERVER['REQUEST_URI'], 'lNumI');
+		if(!isset($_SESSION["Username"])){
+				header("location: Login.php");
+		}
 		//$_SERVER['REQUEST_URI'] = modifyGetParameterInURI($_GET,'lNumI');
-		
 		unset($_GET['lNumI']);
 	}
   ?>
